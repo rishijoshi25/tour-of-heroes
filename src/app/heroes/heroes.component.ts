5184ee8332e77b48c1b8fb1c13ecd0701ae54ada
Rishi Joshi <41236926+rishijoshi25@users.noreply.github.com>
2024-01-06 17:06:26 -0500

    Displaying hero list and details of selected hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-heroes',
@@ -7,8 +9,10 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
